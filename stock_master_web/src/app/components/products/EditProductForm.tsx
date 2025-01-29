7dbf8694adc2c1715259e908fd12531a9f0edb3b
MartinpcDev <martinchrispc@hotmail.com>
2025-01-28 20:26:29 -0500

    fix(Frontent): editando texto de los botones de producto

--- stock_master_web/src/app/components/products/EditProductForm.tsx
+++ stock_master_web/src/app/components/products/EditProductForm.tsx
@@ -171,7 +171,7 @@ export const EditProductForm: React.FC<EditProductFormProps> = ({
 					<button
 						type='submit'
 						className='text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'>
-						Editar Proveedor
+						Editar Producto
 					</button>
 				</form>
 			)}
--- stock_master_web/src/app/components/products/ProductForm.tsx
+++ stock_master_web/src/app/components/products/ProductForm.tsx
@@ -158,7 +158,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
 				<button
 					type='submit'
 					className='text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800'>
-					Crear Proveedor
+					Crear Producto
 				</button>
 			</form>
 		</>
